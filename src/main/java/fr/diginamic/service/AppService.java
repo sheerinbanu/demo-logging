a58c44f4afbb8127f53a3568a968c62f361fd03b
Sheerin Banu <sheemuzz@outlook.com>
2024-11-14 13:56:22 +0100

    TP Logging exo3 modifié

--- src/main/java/fr/diginamic/service/AppService.java
+++ src/main/java/fr/diginamic/service/AppService.java
@@ -12,7 +12,12 @@ public class AppService {
         ap.executer("param1");
 
         for (int i = 0; i < 1000; i++) {
-            LOG.debug("This is log message number {}", i);
+            LOG.debug("This is log message number {} that should help us reach the 1KB limit quickly. \" +\n" +
+                    "                      \"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut \" +\n" +
+                    "                      \"labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco \" +\n" +
+                    "                      \"laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in \" +\n" +
+                    "                      \"voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat \" +\n" +
+                    "                      \"non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\"", i);
             
         }
 
