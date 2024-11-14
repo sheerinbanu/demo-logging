b5bd3cec9acf82694cd83f544fc21d1206f15cd6
Sheerin Banu <sheemuzz@outlook.com>
2024-11-14 11:33:33 +0100

    TP Logging exo1 "message bienvenue"

---
+++ src/main/java/TestLogging.java
@@ -0,0 +1,14 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogging {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestLogging.class);
+
+
+    public static void main(String[] args) {
+
+        logger.info("Bienvenue !");
+
+    }
+}
---
+++ src/main/java/fr/diginamic/fr/diginamic/service/AppService.java
@@ -0,0 +1,4 @@
+package fr.diginamic.fr.diginamic.service;
+
+public class AppService {
+}
